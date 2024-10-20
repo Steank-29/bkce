f7d7ffa82069007a076bb5886c88e7ed47e7f195
Sami <samijlassi2909@gmail.com>
2024-10-20 14:16:11 +0200

    image commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import image1 from './assets/bg1.jpg'; // Adjust the paths as necessary
+import image2 from './assets/bg2.jpg';
+import image3 from './assets/bg3.jpg';
 
+const images = [image1, image2, image3];
 
 function App() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  const appStyle = {
+    backgroundImage: `url(${images[currentImageIndex]})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  };
+
   return (
-    <React.Fragment>
-      <Navbar/>
-    </React.Fragment>
-  )
+    <div style={appStyle}>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
